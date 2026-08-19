6bf3c042e40efddf31dd21c60fab2ab819665201
Rafaomm <rafamast18@gmail.com>
2026-08-18 23:01:42 -0300

    feat : Passagem de props entre components parents

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,11 +2,12 @@ import Rafael from "./components/Rafael/Rafael";
 
 export default function App(){
   //Declarar constantes e variaveis
+
   return(
     <div>
         {/**Aqui você cria todo conteúdo HTML de apresentação */ }
         <h1>Meu Componente</h1>
-        <Rafael/>
+        <Rafael sobreNome1="Mastropaolo" sobreNome2="de Macedo"/>
     </div>
   );
 
--- my-app/src/components/Rafael/Rafael.tsx
+++ my-app/src/components/Rafael/Rafael.tsx
@@ -1,6 +1,11 @@
+type TipoProps = {
+    sobreNome1:string;
+    sobreNome2:string;
+}
 
-export default function Rafael(){
+export default function Rafael({sobreNome1,sobreNome2}:TipoProps){
     return(
-        <h2>Rafael</h2>
+        <h2>Rafael {sobreNome1 + " " + sobreNome2}</h2>
+        
     );
 }
