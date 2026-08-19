e3d4b0925908a156d1e11fa808ee1dedf66f2c5f
Yasmin <ydossantoscorporate@gmail.com>
2026-08-18 23:01:02 -0300

    feat : Passagem de props entre componentes parents

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import Yasmin from './components/Yasmin/Yasmin';
+
+
+export default function App() {
+  
+  
+  return (
+    <div>
+      {/** Conteúdo da aplicação */}
+      <h1>Olá, mundo!</h1>
+      <Yasmin sobreNome1="Nunes" sobreNome2="Santos"/>
+    </div>
+  );
+}
--- my-app/src/components/Yasmin/Yasmin.tsx
+++ my-app/src/components/Yasmin/Yasmin.tsx
@@ -1,5 +1,11 @@
-export default function Yasmin(){
+
+type tipoProps = {
+    sobreNome1: string;
+    sobreNome2: string;
+}
+
+export default function Yasmin({ sobreNome1, sobreNome2 }: tipoProps){
     return(
-        <h2>Yasmin</h2>
+        <h2>Yasmin {sobreNome1} + " " + {sobreNome2}</h2>
     );
 }
