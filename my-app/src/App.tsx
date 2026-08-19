3182027dc2796f8fbac5a6a993afbe3f3e04fdd9
felipe-santosdev <contato.felipesantos27@gmail.com>
2026-08-18 21:58:17 -0300

    feat: Limpeza do boiler plate.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,1 @@
-import Felipe from "./components/Felipe/Felipe";
-
-export default function App() {
-
-  //Declarar constantes e variáveis
-  return (
-    <div>
-      <h1>Meu componente</h1>
-      <Felipe/>
-    </div>
-  );
-}
+export default function App() {}
