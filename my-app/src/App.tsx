a03ad3d23bbdd6f338b7ffa5b4c60850b174c9e3
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-08-18 23:01:01 -0300

    feat : Passagem de props entre components parents;

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,12 +3,20 @@ import Ale from "./components/Ale/Ale";
 export default function App() {
   //Declarar constantes e variáveis
 
-  return (
+  const sobreNome1:string = "Carlos";
+  const sobreNome2:string = "de Jesus";
+
+
+
+  return ( 
     <div>
       {/** Aqui você cria todo conteúdo HTML de apresentação. */}
       <h1>Meu Componente</h1>
-      <Ale/>
+      <Ale sobreNome={[sobreNome1,sobreNome2]}/>
     </div>
   );
 
 }
+
+
+{/* <Ale sobreNome={{a:"Carlos",b:"de Jesus"}}/> */}
--- my-app/src/components/Ale/Ale.tsx
+++ my-app/src/components/Ale/Ale.tsx
@@ -1,6 +1,35 @@
 
-export default function Ale(){
+type TipoProps = {
+    sobreNome:Array<string>;
+}
+
+export default function Ale({sobreNome}:TipoProps){
+
+
     return(
-        <h2>Alexandre</h2>
+        <h2>Alexandre {sobreNome[0] + " " + sobreNome[1]}</h2>
+        
     );
 }
+
+
+
+// export default function Ale({sobreNome}:{sobreNome:TipoProps}){
+//     return(
+//         <h2>Alexandre {sobreNome.a + " " + sobreNome.b}</h2>
+        
+//     );
+// }
+
+
+// type TipoProps = {
+//     sobreNome1:string;
+//     sobreNome2:string;
+// }
+
+// export default function Ale({sobreNome1,sobreNome2}:TipoProps){
+//     return(
+//         <h2>Alexandre {sobreNome1 + " " + sobreNome2}</h2>
+        
+//     );
+// }
