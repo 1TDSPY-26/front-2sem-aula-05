1fbb8fbd707a5f56939187f27283434bcb739878
felipe-santosdev <contato.felipesantos27@gmail.com>
2026-08-18 23:08:31 -0300

    feat : Trabalhando com Props e realizado passagem de Props.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,1 +1,10 @@
-export default function App() {}
+import Felipe from "./components/Felipe/Felipe";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Meu Componente</h1>
+      <Felipe sobreNome={a:"Felipe", b:"Kenji"}/>
+    </div>
+  );
+}
--- my-app/src/components/Felipe/Felipe.tsx
+++ my-app/src/components/Felipe/Felipe.tsx
@@ -1,5 +1,8 @@
-export default function Felipe(){
-    return(
-        <h2>Felipe</h2>
-    );
+type TipoProps = {
+  sobreNome1: string;
+  sobreNome2: string;
+};
+
+export default function Felipe({ sobreNome }: { sobreNome: TipoProps }) {
+  return <h2>Felipe {sobreNome.a + " " + sobreNome.b}</h2>;
 }
